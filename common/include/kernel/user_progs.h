b5fd7d77300350ad571b57da5c2ee568a8c341fa
Stefanie Stoppacher <stoppacher@student.tugraz.at>
2024-03-10 19:10:17 +0100

    quick check if the error gone away

--- common/include/kernel/user_progs.h
+++ common/include/kernel/user_progs.h
@@ -3,11 +3,13 @@
 //DO NOT CHANGE THE NAME OR THE TYPE OF THE user_progs VARIABLE!
 char const *user_progs[] = {
 // for reasons of automated testing
-                            "/usr/shell.sweb",
+                            //"/usr/shell.sweb",
 
-                            // "/usr/pc1.sweb",
-                            // "/usr/pc2.sweb",
-                            // "/usr/pc3.sweb",
+                            "/usr/pc1.sweb",
+                            "/usr/pc2.sweb",
+                            "/usr/pc3.sweb",
+                            "/usr/pc4.sweb",
+                            "/usr/pc5.sweb",
                             0
                            };
 
--- userspace/tests/pc2.c
+++ userspace/tests/pc2.c
@@ -5,15 +5,6 @@
 
 int function1(void* simple_argument)
 {
-    // int array[100];
-    // for(int i = 0; i < 100; i++)
-    // {
-    //     array[i] = i;
-    // }
-    // for(int i = 0; i < 100; i++)
-    // {
-    //     printf("%d ", array[i]);
-    // }
     return 0;
 }
 
@@ -34,20 +25,8 @@ int main()
     assert(thread_id != 424242 && "thread_id was not set");
     assert(thread_id != 0 && "UserThread has ID of KernelThread");
 
-    //Test2: 2 Threads
-    // pthread_t thread_id1 = 424242;
-    // pthread_t thread_id2 = 424242;
-    // rv = pthread_create(&thread_id1, NULL, (void * (*)(void *))function2, NULL);
-    // assert(rv == 0 && "Return value on succesful creation should be 0.");
-    // rv = pthread_create(&thread_id2, NULL, (void * (*)(void *))function2, NULL);
-    // assert(rv == 0 && "Return value on succesful creation should be 0.");
-    // assert(thread_id1 != 424242 && "thread_id was not set");
-    // assert(thread_id2 != 424242 && "thread_id was not set");
-    // assert(thread_id1 != thread_id2 && "Two running thread cant have the same id.");
+    printf("pc2 successful!\n");
 
-    // printf("pc2 successful!\n");
-
-    //TODO: nonesene attribute
 
     return 0;
 }
