3e9a7905f0f16a7dde11ff05b245092b1b2fda83
Duc Tai Nguyen <duc.nguyen@student.tugraz.at>
2024-03-13 09:31:46 +0100

    pthread_create stuck in test

--- common/include/kernel/UserThread.h
+++ common/include/kernel/UserThread.h
@@ -7,7 +7,10 @@ class UserProcess;
 class UserThread : public Thread
 {
     public:
-        UserThread(FileSystemInfo* working_dir, ustl::string name, Thread::TYPE type, uint32 terminal_number, Loader* loader, UserProcess* process, size_t tid);
+        UserThread(FileSystemInfo* working_dir, ustl::string name, Thread::TYPE type, uint32 terminal_number,
+                   Loader* loader, UserProcess* process, size_t tid); //first thread
+        UserThread(FileSystemInfo* working_dir, ustl::string name, Thread::TYPE type, uint32 terminal_number,
+                    Loader* loader, UserProcess* process, size_t tid, void *(*func)(void*), void* arg); //thread for pthread_create
         ~UserThread();
         UserProcess* process_;
         void Run();
