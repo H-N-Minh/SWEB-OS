a29c46486c814465f8dddf1ed9aaf6918dfb1f2c
Duc Tai Nguyen <duc.nguyen@student.tugraz.at>
2024-04-11 10:08:57 +0200

    some comments for cow

--- arch/x86/64/include/paging-definitions.h
+++ arch/x86/64/include/paging-definitions.h
@@ -29,7 +29,7 @@ typedef struct
   uint64 ignored_3                 :1;
   uint64 size                      :1; // must be 0
   uint64 ignored_2                 :3;
-  uint64 cow                       :1;
+  uint64 reference_count           :1;
   uint64 page_ppn                  :28;
   uint64 reserved_1                :12; // must be 0
   uint64 ignored_1                 :11;
@@ -144,7 +144,7 @@ typedef struct
   uint64 size                      :1;
   uint64 global                    :1;
   uint64 ignored_2                 :2;
-  uint64 cow                       :1;
+  uint64 reference_count           :1;
   uint64 page_ppn                  :28;
   uint64 reserved_1                :12; // must be 0
   uint64 ignored_1                 :11;
