69a92ba79981ea0997df05062b3ba3c756f8182d
Duc Tai Nguyen <duc.nguyen@student.tugraz.at>
2024-04-20 16:15:27 +0200

    empty commit

--- common/source/kernel/Loader.cpp
+++ common/source/kernel/Loader.cpp
@@ -16,16 +16,16 @@ Loader::Loader(const Loader &src, int32 fd)
   : arch_memory_(src.arch_memory_), fd_(fd), hdr_(0), phdrs_(0), program_binary_lock_("Loader::program_binary_lock_"),
     userspace_debug_info_(0)
 {
-  debug(FORK, "Loader-Copy constructor called.\n");
+    debug(FORK, "Loader-Copy constructor called.\n");
 
-  readHeaders();
+    readHeaders();
 
-  debug ( LOADER,"loadExecutableAndInitProcess: Entry: %zx, num Sections %zx\n",hdr_->e_entry, (size_t)hdr_->e_phnum );
-  if (LOADER & OUTPUT_ADVANCED)
-    Elf::printElfHeader ( *hdr_ );
+    debug ( LOADER,"loadExecutableAndInitProcess: Entry: %zx, num Sections %zx\n",hdr_->e_entry, (size_t)hdr_->e_phnum );
+    if (LOADER & OUTPUT_ADVANCED)
+      Elf::printElfHeader ( *hdr_ );
 
-  if (USERTRACE & OUTPUT_ENABLED)
-    loadDebugInfoIfAvailable();
+    if (USERTRACE & OUTPUT_ENABLED)
+      loadDebugInfoIfAvailable();
 
 }
 
