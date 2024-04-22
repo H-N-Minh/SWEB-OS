4414e5db581c0dd4a1900ca973a68d940479daea
Stefanie Stoppacher <stoppacher@student.tugraz.at>
2024-04-22 12:46:02 +0200

    locking

--- common/include/kernel/user_progs.h
+++ common/include/kernel/user_progs.h
@@ -4,14 +4,16 @@
 char const *user_progs[] = {
 // for reasons of automated testing
                             // "/usr/shell.sweb",
-                            "/usr/pthreadCreateTests.sweb",
+                          
+// Threads -------------------------------------------------------------------
+                            // "/usr/pthreadCreateTests.sweb",
                             // "/usr/pthreadJoinTests.sweb",
 
 
                             // "/usr/userspaceLocksTests.sweb",
 
                             
-                            // "/usr/shell.sweb",
+
 
                             // "/usr/topG.sweb",               // Test top of stack that is reserved for userspace locking 
                             // "/usr/pc4.sweb",
@@ -29,25 +31,22 @@ char const *user_progs[] = {
                             // "/usr/semTests.sweb",
 
 // FORK-------------------------------------------------
-                            // "/usr/forkTests.sweb",
+                            "/usr/forkTests.sweb",
 
 
                         
                             //"/usr/notest.sweb",
                             //"/usr/shell.sweb",
 
-                            //"/usr/pthreadCreateTests.sweb",
+
                             
-                            // "/usr/pthreadJoinTests.sweb",
+                          
                             // "/usr/pthreadCancelTests.sweb",
                             // "/usr/pthreadExitTests.sweb",
                             // "/usr/pthreadDetachTests.sweb",                                        
 
-                            //"/usr/execTests.sweb",
-                            // "/usr/exec3.sweb",           //Exec with many arguments
-                            // "/usr/exec4.sweb",          //Exec with arguments
-                            // "/usr/exec5.sweb",           //Exec without arguments
-                            // "/usr/exec6.sweb",           //Exec with multiple threads
+                            // "/usr/execTests.sweb",
+
 
                             //"/usr/forkExecTests.sweb",  
 
