a7c640c73299ed8694bde03c64bebbfe504a575d
Stefanie Stoppacher <stoppacher@student.tugraz.at>
2024-04-28 11:00:20 +0200

    testing attributes, sleep

--- common/include/kernel/user_progs.h
+++ common/include/kernel/user_progs.h
@@ -7,52 +7,32 @@ char const *user_progs[] = {
                           
 // Threads -------------------------------------------------------------------
                             // "/usr/pthreadCreateTests.sweb",
-                            // "/usr/pthreadJoinTests.sweb",
+                            // "/usr/pthreadJoinDetachTests.sweb",
 
+                            // "/usr/pthreadCancelTests.sweb",
+                            // "/usr/pthreadExitTests.sweb",
 
+//Fork/Exec -------------------------------------------------------------------
+                            // "/usr/waitpidTests.sweb",
+                            // "/usr/forkTests.sweb",
+                            //"/usr/execTests.sweb",
+                            //"/usr/forkExecTests.sweb",  
+
+// UserspaceLocks ------------------------------------------------------------
                             // "/usr/userspaceLocksTests.sweb",
-
-                            "/usr/waitpidTests.sweb",
-
-
-                            // "/usr/topG.sweb",               // Test top of stack that is reserved for userspace locking 
-                            // "/usr/pc4.sweb",
-                            // "/usr/smallpthreadcreate.sweb",
-                            // "/usr/small_cancel_test.sweb",
-                            // "/usr/stateTypeTest.sweb",
-                            // "/usr/spinTest.sweb",
-                            // "/usr/pthreadCreate.sweb",
-// UserSpace Memory -------------------------------------------------
-                            // "/usr/mallocTests.sweb",
-                            //"/usr/growingStackTests.sweb",
-
-// SEM & COND -------------------------------------------------
                             // "/usr/condTests.sweb",
                             // "/usr/semTests.sweb",
 
-// FORK-------------------------------------------------
-                            // "/usr/forkTests.sweb",
-
-
-                        
+//Other tests -----------------------------------------------------------------
+                            "/usr/sleepAndClockTests.sweb",
+                            //"/usr/growingStackTests.sweb",
                             //"/usr/notest.sweb",
-                            //"/usr/shell.sweb",
+                            // "/usr/topG.sweb",               // Test top of stack that is reserved for userspace locking 
 
 
-                            
-                          
-                            // "/usr/pthreadCancelTests.sweb",
-                            // "/usr/pthreadExitTests.sweb",
-                            // "/usr/pthreadDetachTests.sweb",                                        
 
-                            //"/usr/execTests.sweb",
+ 
 
-
-                            //"/usr/forkExecTests.sweb",  
-
-                            // "/usr/userspaceLocksTests.sweb",
-
-                            // "/usr/sleepAndClockTests.sweb",
 
                             0
                            };
