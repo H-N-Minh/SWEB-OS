86af25bd72fb13fed264f3b39d1e2ba690a0a1ba
Nhat Minh Hoang <nhat.hoang@student.tugraz.at>
2024-06-15 00:13:44 +0200

    all shared passed

--- userspace/tests/mmapSharedTests/shared8.c
+++ userspace/tests/mmapSharedTests/shared8.c
@@ -19,7 +19,7 @@ int shared8() {
     /////////////////////////////////////////////// TEST 8 ////////////////////////////////////////////////
     // Child munmaps the first page, writes to the other 2 pages, then munmaps the other 2 pages. child should exit successfully
     void* addr = MAP_FAILED;
-    int num_pages = 10;
+    int num_pages = 20;
     addr = mmap(NULL, num_pages * 4096, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (addr == MAP_FAILED) {
         printf("mmap 8 failed\n");
@@ -32,28 +32,28 @@ int shared8() {
     child_pid = fork();
     if (child_pid == 0) {
         // munmap the first page
-        printf("child munmaping first page\n");
+        // printf("child munmaping first page\n");
         if (munmap(addr, 4096))
         {
             printf("munmap 8 failed\n");
             return -1;
         }
         // write st to the other 2 pages, to shows that they are still accessible
-        printf("child writing to the other 2 pages\n");
+        // printf("child writing to the other 2 pages\n");
         size_t temp = (size_t) addr + 4096;
-        for (int i = 1; i < (num_pages - 1); i++) {
+        for (int i = 0; i < (num_pages - 1); i++) {
             memcpy((char*) temp, st, strlen(st) + 1);
             temp += 4096;
         }
         temp = (size_t) addr + 4096;
-        printf("child munmaping the other 2 pages\n");
+        // printf("child munmaping the other 2 pages\n");
         // munmap the other 2 pages
         if (munmap((void*) temp, (num_pages - 1) * 4096))
         {
             printf("munmap 8 failed\n");
             return -1;
         }
-        printf("child exiting\n");
+        // printf("child exiting\n");
         exit(0);    // child should still exit successfully
     } 
     else 
@@ -76,9 +76,9 @@ int shared8() {
         temp += 4096;
     }
     // reset the file data and test munmap
-    printf("parent writing to the page\n");
+    // printf("parent writing to the page\n");
     memcpy(addr, "can u see me now", strlen("can u see me now") + 1);
-    printf("parent munmaping the page\n");
+    // printf("parent munmaping the page\n");
     if (munmap(addr, num_pages * 4096))
     {
         printf("munmap 8 failed\n");
